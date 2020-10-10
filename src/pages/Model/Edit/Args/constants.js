39f6d385e589abef6492a1e122cfb81977d45bb7
jqlian1101 <jq.lian@foxmail.com>
2020-10-10 14:19:10 +0800

    模型参数表，添加名称

--- src/pages/Model/Edit/Args/constants.js
+++ src/pages/Model/Edit/Args/constants.js
@@ -1,20 +1,26 @@
 export const TEMP_FIELD_DICT = {
     // "id": "车间连接id",
     // "modelId": "模型ID",
-    "coupDef": { name: "缓冲器形式定义", dict: { 1: '描点法', 2: '分段函数', 3: '本构' } },
+    coupDef: {
+        name: "缓冲器形式定义",
+        dict: { 1: "描点法", 2: "分段函数", 3: "本构" }
+    },
     // "coupname": { name: "缓冲器(钩缓)类型名称" },
-    "couRoute": { name: "缓冲器行程" },
-    "selas": { name: "橡胶轴承最大变形量" },
-    "kelas": { name: "橡胶轴承刚度" },
-    "sprot": { name: "过载保护塑性变形量" },
-    "fprot": { name: "过载保护作用力" },
-    "skanclim": { name: "防爬器空行程" },
-    "sanclim": { name: "防爬器行程" },
-    "fanclim": { name: "防爬器作用力" },
-    "sYkg1": { name: "压溃管1行程" },
-    "fYkg11": { name: "压溃管1作用力1" },
-    "fYkg12": { name: "压溃管1作用力2" },
-    "sYkg2": { name: "压溃管2行程" },
-    "fYkg21": { name: "压溃管2作用力2" },
-    "fYkg22": { name: "压溃管2作用力2" }
-}
+    mName: { name: "缓冲器名称" },
+    couRoute: { name: "缓冲器行程" },
+    fName: { name: "缓冲器名称" },
+    selas: { name: "橡胶轴承最大变形量" },
+    kelas: { name: "橡胶轴承刚度" },
+    sprot: { name: "过载保护塑性变形量" },
+    fprot: { name: "过载保护作用力" },
+    skanclim: { name: "防爬器空行程" },
+    sanclim: { name: "防爬器行程" },
+    fanclim: { name: "防爬器作用力" },
+    yName: { name: "压溃管名称" },
+    sYkg1: { name: "压溃管1行程" },
+    fYkg11: { name: "压溃管1作用力1" },
+    fYkg12: { name: "压溃管1作用力2" },
+    sYkg2: { name: "压溃管2行程" },
+    fYkg21: { name: "压溃管2作用力2" },
+    fYkg22: { name: "压溃管2作用力2" }
+};
